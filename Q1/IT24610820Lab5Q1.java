0a628fcc8dc8bb3c1ee07c04e8f9fd9e49629dc2
IT24610820 <it24610820@my.sliit.lk>
2024-11-29 14:36:37 +0530

    Add files via upload

---
+++ Q1/IT24610820Lab5Q1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class IT24610820Lab5Q1{
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+   
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        
+        System.out.print("Enter the third number: ");
+        int num3 = scanner.nextInt();
+        
+       
+        int smallest = num1;
+        int largest = num1;
+        
+        
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+        
+        if (num2 > largest) {
+            largest = num2;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+        
+        
+        System.out.println("The smallest number is: " + smallest);
+        System.out.println("The largest number is: " + largest);
+        
+       
+        scanner.close();
+    }
+}
